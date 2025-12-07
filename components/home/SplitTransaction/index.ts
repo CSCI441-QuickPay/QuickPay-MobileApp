49c92859a9e26294fd415f24fcf2ffcc8bcc4175
SeanglongLim <167268744+Limseanglong9678@users.noreply.github.com>
2025-12-06 20:27:10 -0600

    Add documentation comments to index.ts
    Added documentation comments for team contributions and unit tests.

--- components/home/SplitTransaction/index.ts
+++ components/home/SplitTransaction/index.ts
@@ -1,3 +1,10 @@
+/**
+ * written by: Team QuickPay
+ * tested by: Team QuickPay
+ * debugged by: Team QuickPay
+ * Notes: Contributions were shared, see GitHub history for commit details.
+ * Unit Tests for UserSyncService
+ */
 export { default as SplitTransactionModal } from './SplitTransactionModal';
 export { default as SplitCreateView } from './SplitCreateView';
 export { default as SplitConfirmationView } from './SplitConfirmationView';
