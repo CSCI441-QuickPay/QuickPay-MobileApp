3e57274d6a477278e051c220e4e01795e4fa9b48
SeanglongLim <167268744+Limseanglong9678@users.noreply.github.com>
2025-12-06 20:30:07 -0600

    Add documentation comments to Header.tsx
    Added documentation comments for team contributions and unit tests.

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -1,3 +1,10 @@
+/**
+ * written by: Team QuickPay
+ * tested by: Team QuickPay
+ * debugged by: Team QuickPay
+ * Notes: Contributions were shared, see GitHub history for commit details.
+ * Unit Tests for UserSyncService
+ */
 import React, { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
