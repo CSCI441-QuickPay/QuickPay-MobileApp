ecdce78dd956489cfe7b3dd6f2053a8ee0be3e1b
SeanglongLim <167268744+Limseanglong9678@users.noreply.github.com>
2025-12-06 20:39:41 -0600

    Add documentation comments to eslint.config.js
    Added documentation comments for team contributions and notes.

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,10 @@
+/**
+ * written by: Team QuickPay
+ * tested by: Team QuickPay
+ * debugged by: Team QuickPay
+ * Notes: Contributions were shared, see GitHub history for commit details.
+ * Unit Tests for UserSyncService
+ */
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
