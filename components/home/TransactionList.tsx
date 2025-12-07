35caab7ee0c345e3166c7522d92afbdfd518bf4a
SeanglongLim <167268744+Limseanglong9678@users.noreply.github.com>
2025-12-06 20:30:51 -0600

    Add documentation for TransactionList component
    Added documentation comments for team contributions and unit tests.

--- components/home/TransactionList.tsx
+++ components/home/TransactionList.tsx
@@ -1,3 +1,10 @@
+/**
+ * written by: Team QuickPay
+ * tested by: Team QuickPay
+ * debugged by: Team QuickPay
+ * Notes: Contributions were shared, see GitHub history for commit details.
+ * Unit Tests for UserSyncService
+ */
 import { useState } from "react";
 import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
