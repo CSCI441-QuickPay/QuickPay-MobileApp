9ec972ee8dc2cc12960b5db57f037280d988ee7e
SeanglongLim <167268744+Limseanglong9678@users.noreply.github.com>
2025-12-06 20:40:01 -0600

    Enhance comments in jest.setup.js
    Added documentation comments for team contributions and test notes.

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,11 @@
 /**
+ * written by: Team QuickPay
+ * tested by: Team QuickPay
+ * debugged by: Team QuickPay
+ * Notes: Contributions were shared, see GitHub history for commit details.
+ * Unit Tests for UserSyncService
+ */
+/**
  * Jest Setup File
  *
  * This file runs before all tests to set up the testing environment,
