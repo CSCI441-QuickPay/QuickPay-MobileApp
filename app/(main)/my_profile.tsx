17887c85818568d199cde9f0f98e78602e1c42a9
SeanglongLim <167268744+Limseanglong9678@users.noreply.github.com>
2025-12-06 20:19:42 -0600

    Update my_profile.tsx

--- app/(main)/my_profile.tsx
+++ app/(main)/my_profile.tsx
@@ -1,3 +1,10 @@
+/**
+ * written by: Team QuickPay
+ * tested by: Team QuickPay
+ * debugged by: Team QuickPay
+ * Notes: Contributions were shared, see GitHub history for commit details.
+ * Unit Tests for UserSyncService
+ */
 // React / Expo imports
 import * as ImagePicker from "expo-image-picker";
 import { useEffect, useState } from "react";
