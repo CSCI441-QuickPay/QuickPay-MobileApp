533ced2494a121cbd8e4cad4eed7d9fa4b258547
SeanglongLim <167268744+Limseanglong9678@users.noreply.github.com>
2025-12-06 20:38:51 -0600

    Enhance documentation in profileUtils.ts
    Added documentation comments for authorship and notes.

--- utils/profileUtils.ts
+++ utils/profileUtils.ts
@@ -1,4 +1,11 @@
 /**
+ * written by: Team QuickPay
+ * tested by: Team QuickPay
+ * debugged by: Team QuickPay
+ * Notes: Contributions were shared, see GitHub history for commit details.
+ * Unit Tests for UserSyncService
+ */
+/**
  * Utility functions for generating profile initials and colors
  */
 
