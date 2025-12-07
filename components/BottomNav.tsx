6c5d11cf46c151c303ad95b164d01f2b47e787dc
SeanglongLim <167268744+Limseanglong9678@users.noreply.github.com>
2025-12-06 20:34:48 -0600

    Add documentation comments to BottomNav component
    Added documentation comments for team contributions and unit tests.

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -1,3 +1,10 @@
+/**
+ * written by: Team QuickPay
+ * tested by: Team QuickPay
+ * debugged by: Team QuickPay
+ * Notes: Contributions were shared, see GitHub history for commit details.
+ * Unit Tests for UserSyncService
+ */
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import resolveConfig from 'tailwindcss/resolveConfig';
